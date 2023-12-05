c456f7b11e6891265602195fa3f2bd40f6dc926b
Aprecity <kathrynfisher3700@gmail.com>
2023-12-05 17:09:16 -0500

    adding to home page

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -10,7 +10,7 @@ function Nav() {
     <nav className="navbar">
   <div className="container-fluid">
     
-      <a className="navbar-brand" href="#"><img src={stag}></img>StagCrest Creations</a>
+      <a className="navbar-brand" href="#"><img src={stag} id='stag'></img>StagCrest Creations</a>
         <li className="nav-link active"><Link to="/" className={currentPage === '/' ? 'nav-link active' : 'nav-link'}>Home</Link></li>
         <li className="nav-link"><Link to="/About" className={currentPage === '/About' ? 'nav-link active' : 'nav-link'}>About</Link></li>
         <li className="nav-link"><Link to="/Projects" className={currentPage === '/Projects' ? 'nav-link active' : 'nav-link'}>Projects</Link></li>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
-import './Home.css'
+import '../styles/Home.css'
+import icon from '../assets/kathryn-icon.png'
+import paint from '../assets/paint.png'
+
+
 function Home() {
 
     return (
         <div>
-           <p>Home page</p> 
+            <div>
+           <img src={icon} id='icon'></img> 
+           </div>
+           <div>
+            <img src={paint} id='paint'></img>
+            <h1>Hello!</h1>
+           </div>
         </div>
     )
 }
