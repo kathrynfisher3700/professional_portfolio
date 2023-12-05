ba422758376d77f412e16de06932a377a16b193a
Aprecity <kathrynfisher3700@gmail.com>
2023-12-05 17:50:44 -0500

    home page created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,22 @@
-import '../styles/Home.css'
-import icon from '../assets/kathryn-icon.png'
-import paint from '../assets/paint.png'
-
+import "../styles/Home.css";
+import icon from "../assets/kathryn-icon.png";
+import paint from "../assets/paint.png";
 
 function Home() {
-
-    return (
-        <div>
-            <div>
-           <img src={icon} id='icon'></img> 
-           </div>
-           <div>
-            <img src={paint} id='paint'></img>
-            <h1>Hello!</h1>
-           </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-auto">
+          <img src={icon} id="icon"></img>
         </div>
-    )
+        <div className="col-md-auto">
+          <img src={paint} id="paint"></img>
+          <p>I'm Kathryn. Creative Designer and Teacher. 
+            <br></br>Elevating Experiences through Innovative Design</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
