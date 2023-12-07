b1a1b3991ffb460d2ba51edbcec99db0c6c1d8e9
Aprecity <kathrynfisher3700@gmail.com>
2023-12-07 18:37:04 -0500

    added buttons to About page, working on css

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,30 @@
-function About() {
+import "../styles/About.css";
+import "bootstrap/dist/css/bootstrap.css";
 
-    return (
-        <div>
-           <p>About</p> 
+function About() {
+  return (
+    <div>
+      <div>
+        <div className="aboutMe">
+          <h1>Kathryn Dougherty</h1>
+          <h2>Education</h2>
+          <ul>
+            <li>Full Stack Web Development 2023</li>
+            <li>Bachelor of Arts: Criminal Justice and Corrections</li>
+          </ul>
+          <h2>Background</h2>
         </div>
-    )
+      </div>
+      <div className="buttonHolder">
+        <button className="button custom-btn btn-4">Resume</button>
+
+        <button className="button custom-btn btn-4">GitHub</button>
+
+        <button className="button custom-btn btn-4">LinkedIn</button>
+      </div>
+      <div></div>
+    </div>
+  );
 }
 
 export default About;
